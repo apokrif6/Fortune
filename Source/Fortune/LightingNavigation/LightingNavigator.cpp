683ca4965aa0bfaff07fabe519aad3884205d15b
apokrif6 <j.berenchan@gmail.com>
2022-12-07 19:22:18 +0100

    Create basic login for lighting navigator trigger

---
+++ Source/Fortune/LightingNavigation/LightingNavigator.cpp
@@ -0,0 +1,67 @@
+﻿#include "LightingNavigator.h"
+
+#include "TexAlignTools.h"
+#include "Components/BoxComponent.h"
+#include "Fortune/FortuneCharacter.h"
+
+ALightingNavigator::ALightingNavigator()
+{
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("Box collision"));
+	Trigger->SetCollisionProfileName(TEXT("Trigger"));
+	Trigger->SetupAttachment(RootComponent);
+}
+
+void ALightingNavigator::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Trigger->OnComponentBeginOverlap.AddDynamic(this, &ALightingNavigator::OnTrigger);
+}
+
+void ALightingNavigator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ALightingNavigator::Appear()
+{
+	MarkAsTriggered();
+	
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, TEXT("T"));
+	}
+}
+
+void ALightingNavigator::Disappear()
+{
+	
+}
+
+void ALightingNavigator::MarkAsTriggered()
+{
+	CanBeTriggered = false;
+
+	GetWorldTimerManager().SetTimer(ActivityHandler, this, &ALightingNavigator::MarkAsReady, 5.f);
+}
+
+void ALightingNavigator::MarkAsReady()
+{
+	CanBeTriggered = true;
+}
+
+
+void ALightingNavigator::OnTrigger(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (Cast<AFortuneCharacter>(OtherActor) == nullptr)
+	{
+		return;
+	}
+	
+	if (CanBeTriggered)
+	{
+		Appear();
+	}
+}
+
---
+++ Source/Fortune/LightingNavigation/LightingNavigator.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "Engine/StaticMeshActor.h"
+#include "LightingNavigator.generated.h"
+
+UCLASS()
+class FORTUNE_API ALightingNavigator : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+	ALightingNavigator();
+
+	UPROPERTY(EditAnywhere, Category = TriggerCollider)
+	UBoxComponent* Trigger;
+
+	UFUNCTION()
+	void OnTrigger(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	virtual void BeginPlay() override;
+	
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	FTimerHandle ActivityHandler;
+	
+	bool CanBeTriggered = true;
+
+	void MarkAsTriggered();
+
+	void MarkAsReady();
+	
+	void Appear();
+
+	void Disappear();
+};
